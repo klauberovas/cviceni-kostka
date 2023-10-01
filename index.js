ae2ff955c96740705191438eb0a7754e2905d9a2
Šárka Klauberová <sarkaa.novakova@seznam.cz>
2023-10-01 13:55:57 +0200

    Done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+let number = 1;
 
+document.addEventListener('keydown', () => {
+  let side = document.querySelector('.dice');
+  if (number < 6) {
+    number += 1;
+    return (side.src = `img/side${number}.svg`);
+  }
+  number = 1;
+  return (side.src = `img/side${number}.svg`);
+});
